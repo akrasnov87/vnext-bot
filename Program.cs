5788a5d3916bc20d872c16dfc91aa35cdf4681cb
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-09-28 16:49:02 +0300

    обновление

--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.10.3
 
+/**
+ * https://habr.com/ru/post/281296/
+ * https://docs.microsoft.com/ru-ru/azure/bot-service/dotnet/bot-builder-dotnet-sdk-quickstart?view=azure-bot-service-4.0&tabs=vs
+ * https://github.com/Microsoft/BotFramework-Emulator/releases/tag/v4.10.0
+ */
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
