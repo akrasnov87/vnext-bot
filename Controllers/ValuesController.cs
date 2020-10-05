d31f9b3fc82aa5870704024bf3f1b92ea5c9793e
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-10-05 18:04:12 +0300

    update

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using vNextBot.Bots;
+using vNextBot.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace vNextBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Проверка на русские символы";
+        }
+    }
+}
